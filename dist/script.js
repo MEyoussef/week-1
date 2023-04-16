71994aae1ee07c336e62fbf0c239256685749887
Me <118435243+MEyoussef@users.noreply.github.com>
2023-04-16 14:04:18 -0700

    three

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,16 @@
 "use strict";
+// 2
 function calculate(numOne, numTwo) {
     return numOne + numTwo;
 }
 console.log(calculate(10, 20)); // 30
 // console.log(calculate("10", "20")); // We Don't Need This To Work
 console.log(calculate(+true, +true)); // 2
+// 3
+function printInfo(valueOne, valueTwo) {
+    return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+}
+console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
+console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
+console.log(printInfo(true, [1, 2, 3])); // We Don't Need This To Work
+//# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+// 2
 function calculate(numOne: number, numTwo: number): number {
     return numOne + numTwo;
   }
@@ -5,3 +6,12 @@ function calculate(numOne: number, numTwo: number): number {
   console.log(calculate(10, 20)); // 30
   // console.log(calculate("10", "20")); // We Don't Need This To Work
   console.log(calculate(+true, +true)); // 2
+
+// 3
+function printInfo(valueOne: string | number, valueTwo: string | number): string | number {
+  return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+}
+
+console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
+console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
+console.log(printInfo(true, [1, 2, 3])); // We Don't Need This To Work
